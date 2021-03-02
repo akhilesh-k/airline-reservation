49e3f7a3c64dd20edbdda0ed037859e5b0a65c23
Akhilesh Kumar <akhilesh_k@outlook.com>
2021-03-02 16:48:06 +0530

    Project Initialization

---
+++ README.md
@@ -0,0 +1,3 @@
+### Airline Reservation System
+
+Contains the Server and Client files for Airline Reservation System. Spring Boot, JPA, PostgresSQL and Vue based project for implementing the Airline Reservation System.
---
+++ airline-reservation-server/src/test/java/com/example/airlinereservationserver/AirlineReservationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.airlinereservationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlineReservationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
