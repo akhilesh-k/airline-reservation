089d52731d9369a1fe8ad607484854b5fefa1116
Akhilesh Kumar <akhilesh_k@outlook.com>
2021-03-04 13:43:18 +0530

    added global font Roboto/open-sans

--- airline-reservation-client/src/App.vue
+++ airline-reservation-client/src/App.vue
@@ -5,7 +5,11 @@
 </template>
 
 <style lang="scss">
+
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans&family=Roboto:wght@100;300;400;500;700&display=swap');
 *{
   margin: 0px;
+  font-family: 'Open Sans', sans-serif;
+font-family: 'Roboto', sans-serif;
 }
 </style>
