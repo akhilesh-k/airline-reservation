aa4daa958b0fad1d8e19a826f682da384659365d
Akhilesh Kumar <akhilesh_k@outlook.com>
2021-03-05 23:26:48 +0530

    added flight booking history detials functionality

---
+++ airline-reservation-server/airline-reservation-booking-details-service/src/main/java/com/quinbay/airlinereservationbookingdetails/AirlineReservationBookingDetailsApplication.java
@@ -0,0 +1,13 @@
+package com.quinbay.airlinereservationbookingdetails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlineReservationBookingDetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlineReservationBookingDetailsApplication.class, args);
+	}
+
+}
