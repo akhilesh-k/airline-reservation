c75f8cdbe60bfb2e6ae2a2ca1ed24db06ec78add
Akhilesh Kumar <akhilesh_k@outlook.com>
2021-03-08 13:18:14 +0530

    fixing the conflicts

--- airline-reservation-server/airline-registration-auth/src/main/java/com/example/FlightBooking/service/UserSignInService.java
+++
@@ -1,8 +0,0 @@
-package com.example.FlightBooking.service;
-
-import com.example.FlightBooking.dto.UserSignInRequestDTO;
-import com.example.FlightBooking.dto.UserSignInResponseDTO;
-
-public interface UserSignInService {
-    boolean validate(UserSignInRequestDTO userSignInRequestDTO);
-}
